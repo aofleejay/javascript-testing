37b949223df7f03c1b966343cd249d52759dd8ac
aofleejay <aofleejay@hotmail.com>
2019-07-05 03:48:02 +0700

    :sparkles: Add twiiter testing workshop.

---
+++ twitter/src/Twitter.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import './Twitter.css'
+
+class Twitter extends Component {
+  state = {
+    tweets: [],
+    inputText: '',
+  }
+
+  onTextChange = e => {
+    this.setState({ inputText: e.target.value })
+  }
+
+  submitTweet = e => {
+    e.preventDefault()
+    this.setState(
+      { tweets: [...this.state.tweets, { text: this.state.inputText }] },
+      () => {
+        this.setState({ inputText: '' })
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div id="twitter-board" className="twitter-board">
+        <form id="tweet-form" onSubmit={this.submitTweet}>
+          <input
+            id="tweet-text"
+            className="tweet-text"
+            type="text"
+            value={this.state.inputText}
+            onChange={this.onTextChange}
+          />
+          <input
+            id="tweet-button"
+            className="tweet-button"
+            type="submit"
+            value="tweet"
+          />
+        </form>
+        {this.state.tweets.map((tweet, index) => {
+          return (
+            <p className="tweet-item" key={index}>
+              {tweet.text}
+            </p>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Twitter
---
+++ twitter/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Twitter from './Twitter'
+
+ReactDOM.render(<Twitter />, document.getElementById('root'))
